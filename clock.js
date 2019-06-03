7553baeda1eb9008017acdaecb59a1830962d71c
TheLuna <50689544+TheLuna@users.noreply.github.com>
2019-06-02 19:48:04 -0700

    changes for the CSS

--- clock.js
+++ clock.js
@@ -1,12 +1,13 @@
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle = clockContainer.querySelector("h1");
+const clock = document.querySelector(".js-clock");
 
 function getTime(){
     const date = new Date();
-    const minutes = date.getMinutes();
+    const min = date.getMinutes();
     const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours } : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
+    const ampmhour = hours > 12 ? hours - 12 : hours;
+    const ampm = hours > 12 ? "PM" : "AM";
+
+    clock.innerText = `${ampmhour < 10 ? `0${ampmhour}` : ampmhour}:${min < 10 ? `0${min}` : min} ${ampm}`;
 }
 
 function init(){
